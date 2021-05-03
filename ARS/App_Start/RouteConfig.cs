c56cebe5ec96183882053a45882e892df0ff882f
TechstarMahesh <mistermaheshsharma@gmail.com>
2021-05-03 10:56:24 +0530

    Videos 4- Airline Reservation System On ASP.Net MVC_ Admin Panel Part I(720P_HD)

--- ARS/App_Start/RouteConfig.cs
+++ ARS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ARS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
