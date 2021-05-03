85338586832c4454313048870f1f666a55a8b26b
TechstarMahesh <mistermaheshsharma@gmail.com>
2021-05-03 09:09:59 +0530

    User Create User Login User wrong id possword is error this is show wrong user and password All balenced.
    User page not created

--- ARS/Controllers/AdminController.cs
+++ ARS/Controllers/AdminController.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ARS.Models;
 
 namespace ARS.Controllers
 {
     public class AdminController : Controller
     {
+        ContextCS c = new ContextCS();
         // GET: Admin
         public ActionResult Index()
+        {
+            return View();
+        }
+        //[HttpGet]
+        public ActionResult AdminLogin()
+        {
+            if (Session["admin"]!=null)
+            {
+                return RedirectToAction("Deshbord");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(AdminLogin l)
+        {
+            var x = c.AdminLogins.Where(a => a.AdminName == l.AdminName && a.AdminPassword == l.AdminPassword).FirstOrDefault();
+            if (x!=null)
+            {
+                Session["admin"] = l.AdminName;
+                return RedirectToAction("Deshbord");
+            }
+            else
+            {
+                ViewBag.login = "Con not redirect to Dashbord you are entered wrong password";
+            }
+            return View();
+        }
+        public ActionResult Deshbord()
         {
             return View();
         }
--- ARS/Models/ContextCS.cs
+++ ARS/Models/ContextCS.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using ARS.Models;
 
 namespace ARS.Models
 {
